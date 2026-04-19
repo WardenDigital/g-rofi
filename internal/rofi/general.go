ee54a329c75c7119c5a2a837df7dd02ae494a00b
WardenDigital <wardendigital@proton.me>
2026-04-19 19:50:51 +0200

    attempt to fix config

--- internal/rofi/general.go
+++ internal/rofi/general.go
@@ -1,22 +1,45 @@
 package rofi
 
 import (
+	"fmt"
 	"io"
+	"os"
 	"os/exec"
 )
 
+var configFileName = "config.rasi"
 var inlineTheme string = "inlinePrompt.rasi"
 
 func createDmenuCommand(title string, r io.Reader) *exec.Cmd {
-	cmd := exec.Command("rofi", "-dmenu", "-p", title)
+	cmd := exec.Command("rofi", "-config", getConfig(), "-dmenu", "-p", title)
 	cmd.Stdin = r
 
 	return cmd
 }
 
 func createInlineDmenuCommand(title string, r io.Reader) *exec.Cmd {
-	cmd := exec.Command("rofi", "-dmenu", "-p", title, "-theme", inlineTheme)
+	cmd := exec.Command("rofi", "-config", getConfig(), "-dmenu", "-p", title, "-theme", getTheme(inlineTheme))
 	cmd.Stdin = r
 
 	return cmd
+}
+
+func getConfigPath() string {
+	if path := os.Getenv("G_ROFI_CONFIG"); path != "" {
+		return path
+	}
+
+	if path := os.Getenv("THEME_DIR"); path != "" {
+		return path
+	}
+
+	return "."
+}
+
+func getConfig() string {
+	return fmt.Sprintf("%s/%s", getConfigPath(), configFileName)
+}
+
+func getTheme(name string) string {
+	return fmt.Sprintf("%s/%s", getConfigPath(), name)
 }
