34c1e11c2beb707b6abc75aa836f2c39c012b6e8
WardenDigital <wardendigital@proton.me>
2026-04-19 19:39:58 +0200

    oops

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -15,12 +15,13 @@ in
 
     package = lib.mkOption {
       type = lib.types.package;
+      default = flake.packages.${pkgs.system}.default;
       description = "The g-rofi package to install.";
     };
 
     configPath = lib.mkOption {
       type = lib.types.nullOr lib.types.path;
-      default = flake.packages.${pkgs.system}.default;
+      default = null;
       description = "Custom path to the rofi config file.";
     };
   };
@@ -29,7 +30,8 @@ in
     home.packages = [ cfg.package ];
 
     home.sessionVariables = {
-      G_ROFI_CONFIG = if cfg.configPath != null then "${cfg.configPath}" else "";
+      G_ROFI_CONFIG =
+        if cfg.configPath != null then "${cfg.configPath}" else "${cfg.package}/share/config";
     };
   };
 }
